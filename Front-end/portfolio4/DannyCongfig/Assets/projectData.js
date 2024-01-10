8c9ca4d76437f2af0687bc68304ec3e764675b12
Danny <david_khart@outlook.com>
2024-01-10 17:48:18 -0500

    additional portfolios

---
+++ Front-end/portfolio4/DannyCongfig/Assets/projectData.js
@@ -0,0 +1,33 @@
+const projectData = [{
+    id: 1,
+    title: "Python fMRI Processing",
+    backgroundColor: "#f39c11",
+    image: "Assets/fmri.jpg",
+    description: "Python method to process 4-dimensional fMRI data. Uses linear regression and ICA (Independent Component Analysis) to calculate correlation between activated regions of the brain during activities. ",
+    link: "https://github.com/LeekSwan/fmri-processing",
+    tech: "Python, Scikit-Learn, Pandas, NumPy, Seaborn",
+}, {
+    id: 2,
+    title: "Social Circle",
+    backgroundColor: "#f39c11",
+    image: "Assets/social-circle.jpg",
+    description: "Web app able to calculate size of friend circles. Intended to estimate exposure through secondary contact and further especially in our current situation where we must be careful of social interactions.",
+    link: "https://github.com/LeekSwan/Social-Circle",
+    tech: "Javascript, HTML, CSS, Express.Js, React.Js, SQL",
+}, {
+    id: 3,
+    title: "Solar Tracking System",
+    backgroundColor: "#f39c11",
+    image: "Assets/solar-tracking.jpg",
+    description: "A solar tracking system design intended to accurately locate the position of the sun while also reducing cost of scaling up solar arrays. Implements code for driver motors with improved efficiency. ",
+    link: "https://github.com/LeekSwan/Solar-Tracking-System",
+    tech: "C++, Arduino",
+}, {
+    id: 4,
+    title: "Study Hall",
+    backgroundColor: "#f39c11",
+    image: "Assets/studyhall.jpg",
+    description: "Interactive web app designed to create a suitable study space complete with user authentication, music player, to-do list, scoring system and quotes API. Also integrates Restful back-end server to store user data. ",
+    link: "https://github.com/LeekSwan/Comp_Final_Project",
+    tech: "Javascript, HTML, CSS, Node.Js, jQuery, AJAX.",
+} ]
---
+++ Front-end/portfolio4/DannyCongfig/README.md
@@ -0,0 +1,3 @@
+# LeekSwanV2
+
+Hello! This is my web portfolio repository. Please feel free to reach out!
